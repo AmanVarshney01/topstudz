ea57ddb5125f6f6580d01f9a7b3c2702ebd80293
Aman Varshney <av.amanvarshney11@gmail.com>
2024-07-06 18:17:55 +0530

    Database migrations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import { SignIn } from "@/components/sign-in";
+import { db } from "@/drizzle/db";
+import { users } from "@/drizzle/schema";
 
-export default function Home() {
+export default async function Home() {
+  const result = await db.select().from(users).all();
   return (
     <main>
       <SignIn />
+      <h1>Users</h1>
+      <ul>
+        {result.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
