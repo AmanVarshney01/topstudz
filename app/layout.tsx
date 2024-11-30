a07f9846516a910b804b80b193b38d940ae74703
Aman Varshney <av.amanvarshney11@gmail.com>
2024-11-30 23:38:04 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/react"
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server"
 
 import { cn } from "@/lib/utils"
@@ -38,6 +39,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
